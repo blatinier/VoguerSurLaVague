4d559256ee148461ff90ff4270c2f5deb7e18f9e
Benoît Latinier <benoit@latinier.fr>
2012-10-05 23:46:16 +0200

    ADD - widgets links

---
+++ widgets/links/controller.php
@@ -0,0 +1,9 @@
+<?php
+class links extends Widget {
+    public function __construct($cfg, $mysql_connector) {
+        parent::__construct($cfg, $mysql_connector);
+    }
+    
+    public function _do_($action_name) {
+    }
+}
