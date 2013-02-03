b4ec127b5c9dd7465063eebe818c67be048b4257
Benoît Latinier <benoit@latinier.fr>
2013-02-03 17:28:05 +0100

    CLEAN - old categories no more necessary

--- modules/categories/del/action.php
+++
@@ -1,9 +0,0 @@
-<?php
-$req = get_cat_byId($_GET['id']);
-$res = mysql_fetch_assoc($req);
-
-if(strtolower($_POST['val'])=='oui'){
-	del_cat($_GET['id']);
-	$del = true;
-}
-?>
--- modules/categories/new/action.php
+++
@@ -1,8 +0,0 @@
-<?php
-if($_SESSION['ok'] == 1){
-	if(!empty($_POST['titre']) && !empty($_POST['texte'])){
-		new_cat($_POST['titre'], $_POST['texte'], $_POST['type']);
-		$poste = true;
-	}
-}
-?>
