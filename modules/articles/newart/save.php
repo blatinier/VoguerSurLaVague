55506d50d0826866519a2c7a6fd005f7da99f337
Benoît Latinier <benoit@latinier.fr>
2013-03-17 22:07:54 +0100

    ADD - symlink for img/articles + clean

--- modules/articles/newart/save.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("../../../tools/sql.php");
-$auteur = $_GET['auteur'];
-$titre = $_GET['titre'];
-$cat = $_GET['cat']; 
-$art = $_GET['art'];
-$requete = "UPDATE cache SET auteur='".$auteur."', titre='".$titre."', art='".$art."', cat='".$cat."' WHERE id=1"; 
-mysql_query($requete) or die(mysql_error());
-?>
