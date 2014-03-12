9be67ceed783b817f91639301a059d5b0240a536
Benoît Latinier <benoit@latinier.fr>
2014-03-12 17:51:34 +0100

    fix tags

--- models/Tag.php
+++ models/Tag.php
@@ -4,13 +4,15 @@ class Tag extends Model {
     public $id;
     public $name;
 
-    public function __construct($id, $name, $slug) {
+    public function __construct($id, $name, $slug, $nb=0) {
         $this->id = $id;
         $this->name = $name;
         $this->slug = $slug;
+        $this->nb = $nb;
     }
 
     public static function load($dict) {
-        return new Tag($dict['id'], $dict['name'], $dict['slug']);
+        return new Tag($dict['id'], $dict['name'], $dict['slug'],
+                       $dict['nb']);
     }
 }
--- models/repository/TagRepository.php
+++ models/repository/TagRepository.php
@@ -25,6 +25,20 @@ class TagRepository extends Repository {
         return $tags;
     }
 
+    public function get_all_with_count() {
+        $req = 'SELECT id, name, slug, COUNT(*) AS nb
+        FROM tags t
+        LEFT JOIN article_tags at
+        ON t.id = at.tag_id
+        GROUP BY t.id';
+        $art_sql = $this->mysql_connector->fetchAll($req);
+        $tags = array();
+        foreach($art_sql as $tag_data) {
+            $tags[] = Tag::load($tag_data);
+        }
+        return $tags;
+    }
+
     public function get_all() {
         $req = 'SELECT id, name, slug FROM tags';
         $art_sql = $this->mysql_connector->fetchAll($req);
